3fc6b800235f96b04450af120726d181a1980960
pango88 <hansflame@protonmail.com>
2020-03-08 23:56:33 +0100

    Added fules and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var helmet = require('helmet');
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB =
+var dev_db_url =
   'mongodb+srv://pango:pango@cluster0-l3wzb.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
